1ad402dc0c93e22f6ce123eac59fb1d0106f9ee4
ant_reyn <antreyn@udel.edu>
2022-01-25 17:47:34 -0500

    added js to the projects section

---
+++ starting-files/index.js
@@ -0,0 +1,16 @@
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", reveal);
+function reveal() {
+  for (let i = 0; i < reveals.length; i++) {
+    const windowheight = window.innerHeight;
+    const revealtop = reveals[i].getBoundingClientRect().top;
+    const revealpoint = 0;
+
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
