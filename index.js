7f8211163f6300e3af3339fd0dada58f1eb7eb6e
ant_reyn <antreyn@udel.edu>
2022-01-31 17:18:35 -0500

    added a scroll to top button on the projects page

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const reveals = document.querySelectorAll(".reveal");
+const scrollButton = document.querySelector(".fa-arrow-circle-up");
+const rootElement = document.documentElement;
+const topArrow = document.querySelector(".topArrow");
 
 window.addEventListener("scroll", reveal);
 function reveal() {
@@ -13,4 +16,25 @@ function reveal() {
       reveals[i].classList.remove("active");
     }
   }
+}
+
+const showOnPx = 1200;
+const scrollContainer = () => {
+  return rootElement || document.body;
+};
+
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > showOnPx) {
+    topArrow.classList.remove("hidden");
+  } else {
+    topArrow.classList.add("hidden");
+  }
+});
+
+scrollButton.addEventListener("click", scrollToTop);
+function scrollToTop() {
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 }
